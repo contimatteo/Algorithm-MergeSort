b7aa9298c505e721678926ad660408e8b9b62455
contimatteo <contimatteo1505@gmail.com>
2017-07-01 16:22:02 +0200

    Create executable

--- MC.MergeSort/src/sample/Main.java
+++ MC.MergeSort/src/sample/Main.java
@@ -54,7 +54,6 @@ public class Main extends Application {
     {
         switch(scelta)
         {
-
             case 1:
                 Input.createInput(ARRAY_LENGHT, ARRAY_MAX_VALUE);
                 break;
